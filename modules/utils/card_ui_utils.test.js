b7e70f5b8a940ba97b779d928888905ac1f93500
Vinay Vyas <69166360+vinay-google@users.noreply.github.com>
2023-08-01 00:12:15 -0700

    refactor: Use marked to convert markdown to html (#52)

--- modules/utils/card_ui_utils.test.js
+++ modules/utils/card_ui_utils.test.js
@@ -5,8 +5,29 @@ describe('convertMarkdownToWidgets', () => {
     expect(convertMarkdownToWidgets).toBeDefined();
   });
 
-  it('convertMarkdownToWidgets should return expected output', () => {
-    // const retValue = convertMarkdownToWidgets(markdownText);
-    // expect(false).toBeTruthy();
+  it('should convert a bullet list item to a decorated text widget', () => {
+    const markdownText = '- This is a bullet list item';
+    const expectedResult = [{
+      decoratedText: {
+        text: 'This is a bullet list item',
+        startIcon: {
+          knownIcon: 'STAR'
+        },
+        wrapText: true,
+      }
+    }];
+
+    expect(convertMarkdownToWidgets(markdownText)).toEqual(expectedResult);
+  });
+
+  it('should convert a paragraph of text to a paragraph widget', () => {
+    const markdownText = 'This is a paragraph of text';
+    const expectedResult = [{
+      textParagraph: {
+        text: 'This is a paragraph of text'
+      }
+    }];
+
+    expect(convertMarkdownToWidgets(markdownText)).toEqual(expectedResult);
   });
 });
