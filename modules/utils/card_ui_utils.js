f1ef585acac62811d630d4256bfb90ff255d0c35
Vinay Vyas <69166360+vinay-google@users.noreply.github.com>
2023-08-01 00:12:15 -0700

    refactor: Use marked to convert markdown to html (#52)

--- modules/utils/card_ui_utils.js
+++ modules/utils/card_ui_utils.js
@@ -1,96 +1,33 @@
-/**  Written with the help of AI **/
+import {marked} from 'marked';
+marked.use({headerIds: false, mangle: false});
+
 export default function convertMarkdownToWidgets(markdownText) {
-  const results = [];
-
-  // Split the markdown text into an array of lines
-  const markdownLines = markdownText.split('\n');
-
-  // Iterate over each line
-  markdownLines.forEach((line) => {
-    // Log the current line
+  return markdownText.split(/\r\n|\r|\n/).map((line) => {
     const bulletListRegex = /^[-*+] (.*)/;
     const match = line.match(bulletListRegex);
+
     // If the line matches a bullet list item format, replace with
     // a decorated text with a specific icon
     if (match) {
       const bulletListItemText = match[0].slice(2);
 
-      results.push({
+      return {
         decoratedText: {
-          text: markdownToSimpleHtml(bulletListItemText),
+          text: marked.parseInline(bulletListItemText),
           startIcon: {
             knownIcon: 'STAR',
           },
           wrapText: true,
         },
-      });
+      };
     } else {
       // Otherwise, convert markdown text to the supported HTML tags
       // in the paragraph widget
-      results.push({
+      return {
         textParagraph: {
-          text: markdownToSimpleHtml(line),
+          text: marked.parseInline(line),
         },
-      });
+      };
     }
   });
-
-  return results;
-}
-
-/**  THIS IS WRITTEN WITH HELP OF BARD */
-function markdownToSimpleHtml(markdown) {
-  const regexes = [
-    // Bold
-    {
-      pattern: /\*\*([^\*\*]+)\*\*/g,
-      replacement: '<b>$1</b>',
-    },
-    // Italics
-    {
-      pattern: /_([^_]+)_/g,
-      replacement: '<i>$1</i>',
-    },
-    // Underlined
-    {
-      pattern: /__([^_]+)__/g,
-      replacement: '<u>$1</u>',
-    },
-    // Strikethrough
-    {
-      pattern: /~~([^~]+)~~/g,
-      replacement: '<s>$1</s>',
-    },
-    // Newline
-    {
-      pattern: /\r\n/g,
-      replacement: '<br>',
-    },
-    // Links
-    {
-      pattern: /\[(([^\]]+))\]\((([^)]+))\)/g,
-      replacement: '<a href="$2">$1</a>',
-    },
-    // Newline
-    {
-      pattern: /\n/g,
-      replacement: '<br>',
-    },
-    // Newline
-    {
-      pattern: /\r/g,
-      replacement: '<br>',
-    },
-    // Tab
-    {
-      pattern: /\t/g,
-      replacement: '    ',
-    },
-  ];
-
-  for (const regex of regexes) {
-    markdown = markdown.replace(regex.pattern, regex.replacement);
-  }
-
-  return markdown;
 }
